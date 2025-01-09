bce444e523d48c5821240d82ba6f9ce7c0cfbf2c
sybil-o <pjutkmm4l@relay.firefox.com>
2025-01-09 06:07:01 +0100

    minor nvim changes

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -4,8 +4,17 @@ return {
   config = function()
     require('lualine').setup {
       options = {
-        icons_enabled = true,
         theme = 'oxocarbon',
+        section_separators = { '▢', '▢' }, -- Use square characters for section separators
+        component_separators = { '▢', '▢' }, -- Use square characters for component separators
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
       },
     }
   end,
