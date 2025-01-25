753f6beb565ef134ad7f699702c03549ebc7f431
Kevin Hermann <hermannkevin@gmail.com>
2025-01-25 08:42:23 +0100

    various changes

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -20,3 +20,12 @@ zoxide init fish | source
 thefuck --alias | source
 # better shell history
 atuin init fish | source
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/kevinh/.opam/opam-init/init.fish' && source '/home/kevinh/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
