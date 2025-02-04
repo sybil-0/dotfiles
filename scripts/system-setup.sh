5905f05665a294d85c33dd71f8a14dd337b19289
Kevin Hermann <hermannkevin@gmail.com>
2025-02-04 08:35:31 +0100

    zsh changes

--- scripts/system-setup.sh
+++ scripts/system-setup.sh
@@ -12,5 +12,28 @@ sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
 
 chsh -s /usr/bin/fish
 
-# On macOS and Linux.
+# uv, fast python environment management
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Brave Browser 
+curl -fsS https://dl.brave.com/install.sh | sh
+
+# Zed Editor 
+curl -f https://zed.dev/install.sh | sh
+
+# zsh setup 
+# Define the Oh My Zsh custom plugin directory
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+echo "🔹 Installing Zsh Plugins..."
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+# Install zsh-completions
+git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
+# Install zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search $ZSH_CUSTOM/plugins/zsh-history-substring-search
+# Install zsh-you-should-use
+git clone https://github.com/MichaelAquilina/zsh-you-should-use $ZSH_CUSTOM/plugins/zsh-you-should-use
+# Install zsh-abbr
+git clone https://github.com/olets/zsh-abbr $ZSH_CUSTOM/plugins/zsh-abbr
