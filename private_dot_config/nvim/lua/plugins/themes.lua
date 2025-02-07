fc91ae9b62b0e74fc57aa2c1f3df535f8ee39b12
Kevin Hermann <hermannkevin@gmail.com>
2025-02-07 09:38:44 +0100

    misc

--- private_dot_config/nvim/lua/plugins/themes.lua
+++ private_dot_config/nvim/lua/plugins/themes.lua
@@ -1,25 +1,25 @@
 return {
-	{
-		'EdenEast/nightfox.nvim',
-		config = function() end,
-	},
-	{
-		'nyoom-engineering/oxocarbon.nvim',
-		config = function()
-			vim.o.termguicolors = true
-			vim.o.background = 'dark'
-			vim.cmd.colorscheme 'oxocarbon'
-		end,
-	},
-	{
-		'maxmx03/solarized.nvim',
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		'folke/tokyonight.nvim',
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
+  {
+    'EdenEast/nightfox.nvim',
+    config = function() end,
+  },
+  {
+    'nyoom-engineering/oxocarbon.nvim',
+    config = function()
+      vim.o.termguicolors = true
+      vim.o.background = 'dark'
+      vim.cmd.colorscheme 'oxocarbon'
+    end,
+  },
+  {
+    'maxmx03/solarized.nvim',
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
 }
