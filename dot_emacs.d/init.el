3984239d9f8b47cc948a43af5aaf72dee7217d57
Kevin Hermann <hermannkevin@gmail.com>
2025-03-29 11:56:46 +0100

    updated: emacs; ghostty

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -2,15 +2,48 @@
 ;; PACKAGE MANAGER SETUP ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'package)
-(package-initialize)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(defvar elpaca-installer-version 0.10)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil :depth 1 :inherit ignore
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (<= emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (load "./elpaca-autoloads")))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
 
-(when (< emacs-major-version 29)
-  (unless (package-installed-p 'use-package)
-    (unless package-archive-contents
-      (package-refresh-contents))
-    (package-install 'use-package)))
+(elpaca elpaca-use-package
+  ;; Enable use-package :ensure support for Elpaca.
+  (elpaca-use-package-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; GENERAL EMACS CONFIG ;;
@@ -48,63 +81,73 @@
 (setq auto-revert-verbose nil)
 (show-paren-mode +1)
 
-(setq custom-file "~/.emacs.d/emacs-custom.el")
+(setq custom-file (make-temp-file "emacs-custom"))
 (when (file-exists-p custom-file)
   (load custom-file))
+
+;; set typeface
+(add-to-list 'default-frame-alist '(font . "Cascadia Code 14"))
 
 ;;;;;;;;;;;;;;
 ;; PACKAGES ;;
 ;;;;;;;;;;;;;;
 
-(setq use-package-always-ensures t)
-
-(use-package solarized-theme)
+(setq use-package-always-ensure t)
 
 (use-package catppuccin-theme)
-
 (use-package doom-themes)
-
-(use-package ef-themes)
-
+(use-package solarized-theme)
 (use-package magit)
 
+(use-package ef-themes
+  :ensure t
+  :init (load-theme 'ef-bio t))
+
 (use-package exec-path-from-shell
+  :ensure t
   :init (when (memq window-system '(mac ns x))
 	  (exec-path-from-shell-initialize)))
 
-(use-package nerd-icons
-  )
+(use-package nerd-icons :ensure t)
 
 (use-package doom-modeline
+  :ensure t
   :after (nerd-icons)
   :init
   (column-number-mode +1)
   (doom-modeline-mode 1))
 
 (use-package which-key
+  :ensure t
   :init (which-key-mode 1))
 
 (use-package ace-window
+  :ensure t
   :init (global-set-key (kbd "M-o") 'ace-window))
 
 (use-package paredit
+  :ensure t
   :hook ((clojure-mode cider-repl-mode emacs-lisp-mode) . paredit-mode))
 
 (use-package cider
+  :ensure t
   :bind (("C-x c r" . 'cider-repl-clear-buffer)))
 
 (use-package projectile
+  :ensure t
   :config
   (projectile-mode +1)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 (use-package pet
+  :ensure t
   :config
   (add-hook 'python-base-mode-hook 'pet-mode -10))
 
-(use-package lua-mode)
+(use-package lua-mode :ensure t)
 
 (use-package format-all
+  :ensure t
   :commands format-all-mode
   :hook (prog-mode . format-all-mode)
   :config
@@ -113,15 +156,17 @@
                   ("Shell" (shfmt "-i" "4" "-ci")))))
 
 
-(use-package zig-mode)
+(use-package zig-mode :ensure t)
 
 (use-package haskell-mode
+  :ensure t
   :custom
   (haskell-process-load-or-reload-prompt t))
 
-(use-package yasnippet-snippets)
+(use-package yasnippet-snippets :ensure t)
 
 (use-package yasnippet
+  :ensure t
   :config (yas-global-mode +1))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -131,6 +176,7 @@
 
 ;; Enable vertico
 (use-package vertico
+  :ensure t
   :custom
   (enable-recursive-minibuffers t)
   (vertico-resize t)
@@ -138,13 +184,9 @@
   :init
   (vertico-mode +1))
 
-;; Persist history over Emacs restarts. Vertico sorts by history position.
-(use-package savehist
-  :init
-  (savehist-mode))
-
 ;; Optionally use the `orderless' completion style.
 (use-package orderless
+  :ensure t
   :custom
   (completion-styles '(orderless basic))
   (completion-category-defaults nil)
@@ -152,6 +194,7 @@
 
 ;; Enable rich annotations using the Marginalia package
 (use-package marginalia
+  :ensure t
   :bind (:map minibuffer-local-map
               ("M-A" . marginalia-cycle))
   :init (marginalia-mode))
@@ -159,6 +202,7 @@
 
 ;; Example configuration for Consult
 (use-package consult
+  :ensure t
   :bind (("C-c M-x" . consult-mode-command)
 	 ("C-c h" . consult-history)
          ("C-x C-r" . consult-recent-file)
@@ -190,16 +234,19 @@
   (setq consult-narrow-key "<"))
 
 (use-package embark
+  :ensure t
   :bind
   (("C-," . embark-act)
    ("C-;" . embark-dwim)
    ("C-h B" . embark-bindings)))
 
 (use-package embark-consult
+  :ensure t
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
 (use-package company
+  :ensure t
   :init
   (global-set-key (kbd "C-x C-n") 'company-complete)
   (global-company-mode +1)
@@ -208,9 +255,11 @@
         company-minimum-prefix-length 2))
 
 (use-package company-quickhelp
+  :ensure t
   :config (company-quickhelp-mode))
 
 (use-package tree-sitter
+  :ensure t
   :init
   (setq treesit-language-source-alist
 	'((bash "https://github.com/tree-sitter/tree-sitter-bash")
@@ -260,16 +309,20 @@
 ;; KEYMAPS ;;
 ;;;;;;;;;;;;;
 
-(global-set-key (kbd "<f5>") 'consult-theme)
 (global-set-key (kbd "<f2>") 'menu-bar-mode)
+(global-set-key (kbd "<f5>") 'consult-theme)
+(global-set-key (kbd "<f7>") (lambda () (interactive) (consult-theme 'solarized-dark)))
+(global-set-key (kbd "<f8>") (lambda () (interactive) (consult-theme 'solarized-light)))
+;; improved completion defaults
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
 (global-set-key [remap list-buffers] 'ibuffer)
+;; window and frame management 
 (global-set-key (kbd "M-n") 'scroll-up-command)
 (global-set-key (kbd "M-p") 'scroll-down-command)
 (global-set-key (kbd "C-x t f") 'toggle-frame-fullscreen)
 (global-set-key (kbd "C-x t m") 'toggle-frame-maximized)
-(keymap-set minibuffer-local-map "C-k" 'consult-switch-buffer-kill)
 
+(keymap-set minibuffer-local-map "C-k" 'consult-switch-buffer-kill)
 
 (provide 'init)
 ;;; init.el ends here
